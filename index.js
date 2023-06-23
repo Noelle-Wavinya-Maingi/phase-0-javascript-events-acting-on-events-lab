f9de9f401d199d59629ec3e14de60816371cb523
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-23 16:15:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    let leftValue = dodger.style.left.replace('px', '');
+    let left = parseInt(leftValue, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    let leftValue = dodger.style.left.replace('px', '');
+    let left = parseInt(leftValue, 10);
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'ArrowLeft') {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'ArrowRight') {
+        moveDodgerRight()
+    }
+})
